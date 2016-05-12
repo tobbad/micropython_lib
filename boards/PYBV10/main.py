eec88c7b339ff697a00232c5b3c6477a3214529f
Tobias Badertscher <badi@baerospace.ch>
2016-05-12 20:32:24 +0200

    Debugged accelerator.

--- boards/PYBV10/main.py
+++ boards/PYBV10/main.py
@@ -2,11 +2,30 @@
 # Main file for STM32F4DISCOVERY
 #
 import pyb
-from sx127x import SX127x
+from sx127x import SX127X
+from mma7660 import MMA7660
 from board import sys_config
+
+
+i2c = pyb.I2C(sys_config['mma7660']['i2c_bus'],
+              pyb.I2C.MASTER, baudrate=100000)
+
+vdda = pyb.Pin(sys_config['mma7660']['avdd'], pyb.Pin.OUT_PP)
+
+vdda.low()
+pyb.delay(30)
+vdda.high()
+pyb.delay(30)
+
+accel = MMA7660(i2c, sys_config['mma7660']['i2c_addr'])
+print("Acceleration  (%.3e, %.3e, %.3e)" % accel.xyz())
 
 spi = pyb.SPI(sys_config['sx127x']['spi_bus'], pyb.SPI.MASTER,
               baudrate=600000, polarity=1, phase=1)
 cs = pyb.Pin(sys_config['sx127x']['spi_cs'], pyb.Pin.OUT_PP)
 
 rf = SX127X(spi, cs)
+
+while True:
+    print("%3d, %3d, %3d" % ( accel.x(), accel.y(), accel.z()))
+    pyb.delay(100)
--- sensor/mma7660.py
+++ sensor/mma7660.py
@@ -1,42 +1,14 @@
 """
-Driver for STMicroelectronics L3GD20 gyro for MicroPython.
-
-This driver assumes a SPI device and a chip select (CS) pin given
-to the constructor.
-
-The following example assumes that the CS is connected to the "PC1" pin
-and spi bus 5 is used.
-
->>> from pyb import Pin
->>> cs = Pin('PC1', Pin.OUT_PP, Pin.PULL_NONE)
->>> cs.high()
->>> from pyb import SPI
->>> spi_bus_nr = 5
->>> spi = SPI(spi_bus_nr, SPI.MASTER, baudrate=328125,
-              polarity=1, phase=1, bits=8)
-
->>> from stm_l3gd20 import L3GD20
->>> dev = L3GD20(cs, spi)
-
-Reading angular velocity:
-
->>> wx = d.omega_x()
->>> print(wx)
-875.0
-
-See:
-    STM32Cube_FW_F4_V1.3.0/Drivers/BSP/Components/l3gd20/l3gd20.h
-    STM32Cube_FW_F4_V1.3.0/Drivers/BSP/Components/l3gd20/l3gd20.c
 """
 from i2cspi import COM_I2C
 from multibyte import multibyte
 
-MMA7660_X_ADDR = const(0x01)  # Output Register X
-MMA7660_Y_ADDR = const(0x02)  # Output Register Y
-MMA7660_Z_ADDR = const(0x03)  # Output Register Z
+MMA7660_X_ADDR = const(0x00)  # Output Register X
+MMA7660_Y_ADDR = const(0x01)  # Output Register Y
+MMA7660_Z_ADDR = const(0x02)  # Output Register Z
 
 
-class MMA7660(COM_SPI, multibyte):
+class MMA7660(COM_I2C, multibyte):
     #
     # Debug
     #
@@ -58,6 +30,7 @@ class MMA7660(COM_SPI, multibyte):
 
     def _reg2val(self, reg):
         val =self.read_u8(reg)
+        val &= 0x3F
         val = val-64 if val >=32 else val
         return val
 
